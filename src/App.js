6ffcf42586e71bb38f81998ba1b6df6ebfff202c
Anastasia Mazaeva <mazaeva.fgo@gmail.com>
2023-04-25 17:53:01 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+import TaskList from './components/taskList/taskList'
+import NewTaskForm from './components/newTaskForm/newTaskForm'
+import Footer from './components/footer/footer'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      taskList: [],
+      filter: 'All',
+    }
+  }
+
+  onItemAdded = (text) => {
+    this.setState(({ taskList }) => {
+      const newTask = {
+        id: Math.random(),
+        description: text,
+        date: new Date(),
+        checked: false,
+      }
+      const newTaskList = [...taskList, newTask]
+      return { taskList: newTaskList }
+    })
+  }
+
+  onDeleted = (id) => {
+    this.setState(({ taskList }) => {
+      const newTaskList = taskList.filter((taskItem) => taskItem.id !== id)
+      return { taskList: newTaskList }
+    })
+  }
+
+  toggleCompleted = (id) => {
+    this.setState(({ taskList }) => {
+      const newTaskList = taskList.map((item) => (item.id === id ? { ...item, checked: !item.checked } : { ...item }))
+      return { taskList: newTaskList }
+    })
+  }
+
+  clearCompletedTask = () => {
+    this.setState(({ taskList }) => {
+      const newTaskList = taskList.filter((item) => !item.checked)
+      return { taskList: newTaskList }
+    })
+  }
+
+  filterList = () => {
+    const { taskList, filter } = this.state
+    return taskList.filter(({ checked }) => {
+      const all = filter === 'All'
+      const completed = filter === 'Completed'
+      if (all) {
+        return true
+      }
+      if (completed) {
+        return checked
+      }
+      return !checked
+    })
+  }
+
+  changeFilter = (value) => {
+    this.setState({ filter: value })
+  }
+
+  onSubmitEditTask = (id, text) => {
+    this.setState(({ taskList }) => {
+      const newTaskList = taskList.map((item) => (item.id === id ? { ...item, description: text } : { ...item }))
+      return { taskList: newTaskList }
+    })
+  }
+
+  render() {
+    const { taskList, filter } = this.state
+    const countItemsLeft = taskList.length - taskList.filter((item) => item.checked).length
+
+    return (
+      <section className="todoapp">
+        <header className="header">
+          <h1>Todos</h1>
+          <NewTaskForm onItemAdded={this.onItemAdded} />
+        </header>
+        <section className="main">
+          <TaskList
+            taskList={this.filterList()}
+            onDeleted={this.onDeleted}
+            toggleCompleted={this.toggleCompleted}
+            onSubmitEditTask={this.onSubmitEditTask}
+          />
+          <Footer
+            countItemsLeft={countItemsLeft}
+            clearCompletedTask={this.clearCompletedTask}
+            filter={filter}
+            changeFilter={this.changeFilter}
+          />
+        </section>
+      </section>
+    )
+  }
 }
 
-export default App;
+export default App
